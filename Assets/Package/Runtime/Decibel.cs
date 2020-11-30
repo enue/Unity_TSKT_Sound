5c0be9f94b753a9e667c8a00fd42a2d440b7daa4
enu <otoriden@gmail.com>
2020-11-30 16:30:39 +0900

    Decibel

---
+++ Assets/Package/Runtime/Decibel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TSKT
+{
+    public class Decibel
+    {
+        static public float ToDecibel(float normalizedValue, float min = -80f)
+        {
+            // https://techblog.kayac.com/linear-vs-decibel
+            var decibel = min;
+            if (normalizedValue > 0f)
+            {
+                decibel = 20f * Mathf.Log10(normalizedValue);
+                decibel = Mathf.Max(decibel, min);
+            }
+            return decibel;
+        }
+    }
+}
