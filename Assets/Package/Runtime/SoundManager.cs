9173b4968377c66e7bda7436df8ee0cd599244cf
enue <otoriden@gmail.com>
2024-05-12 17:26:49 +0900

    SoundManager.ascendingPriority

--- Assets/Package/Runtime/SoundManager.cs
+++ Assets/Package/Runtime/SoundManager.cs
@@ -1,24 +1,40 @@
-﻿using UnityEngine;
+﻿#nullable enable
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-#nullable enable
 
 namespace TSKT
 {
     public class SoundManager : SoundPlayer
     {
         readonly static List<SoundManager> instances = new();
-        public static SoundManager? Instance => instances.LastOrDefault();
+        public static SoundManager? Instance { get; private set; }
+
+        [SerializeField]
+        int ascendingPriority;
 
         void OnEnable()
         {
-            instances.Add(this);
+            // priorityが等しいManagerが複数ある場合、新しい方を選択する
+            instances.Insert(0, this);
+            SelectHighestPriorityInstance();
         }
 
         void OnDisable()
         {
             instances.Remove(this);
+            SelectHighestPriorityInstance();
+        }
+
+        void SelectHighestPriorityInstance()
+        {
+            if (instances.Count == 0)
+            {
+                Instance = null;
+                return;
+            }
+            Instance = instances.OrderBy(_ => _.ascendingPriority).First();
         }
     }
 }
