48cb2d7db9f639f4aae3ae5083b9ba733c5a51b4
enue <otoriden@gmail.com>
2024-05-12 17:18:27 +0900

    AudioListenerSelector

---
+++ Assets/Package/Runtime/AudioListenerSelector.cs
@@ -0,0 +1,43 @@
+﻿#nullable enable
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TSKT.MusicManagers
+{
+    public class AudioListenerSelector : MonoBehaviour
+    {
+        readonly static List<AudioListenerSelector> instances = new();
+
+        [SerializeField]
+        AudioListener target = default!;
+
+        [SerializeField]
+        int ascendingPriority;
+
+        void OnEnable()
+        {
+            instances.Add(this);
+            EnableHighestPriorityListener();
+        }
+        void OnDisable()
+        {
+            instances.Remove(this);
+            EnableHighestPriorityListener();
+        }
+
+        static void EnableHighestPriorityListener()
+        {
+            if (instances.Count == 0)
+            {
+                return;
+            }
+            var top = instances.Max(_ => _.ascendingPriority);
+            foreach (var it in instances)
+            {
+                it.target.enabled = top == it.ascendingPriority;
+            }
+        }
+    }
+}
